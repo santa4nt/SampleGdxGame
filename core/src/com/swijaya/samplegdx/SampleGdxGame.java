4331b0c781296d7a8074e15e38f10e5bfb5d9d86
Santoso Wijaya <santoso.wijaya@gmail.com>
2015-05-30 21:46:15 -0700

    Using Game and Screen API.

--- core/src/com/swijaya/samplegdx/SampleGdxGame.java
+++ core/src/com/swijaya/samplegdx/SampleGdxGame.java
@@ -1,17 +1,18 @@
 package com.swijaya.samplegdx;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-public class SampleGdxGame extends ApplicationAdapter {
+public class SampleGdxGame extends Game {
+
+    private static final String TAG = SampleGdxGame.class.getSimpleName();
+
+    private Screen mainScreen;
 
     private Stage stage;
 
@@ -20,10 +21,10 @@ public class SampleGdxGame extends ApplicationAdapter {
 
     @Override
     public void create() {
-        badLogicImg = new Texture("badlogic.jpg");
+        Gdx.app.setLogLevel(Application.LOG_DEBUG);
 
+        badLogicImg = new Texture("badlogic.jpg");
         stage = new Stage(new ScreenViewport());
-        Gdx.input.setInputProcessor(stage);
 
         badLogicActor = new Actor() {
             @Override
@@ -32,24 +33,38 @@ public class SampleGdxGame extends ApplicationAdapter {
             }
         };
         stage.addActor(badLogicActor);
+
+        mainScreen = new ScreenAdapter() {
+
+            @Override
+            public void render(float delta) {
+                Gdx.gl.glClearColor(1, 0, 0, 1);
+                Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+                stage.act(delta);
+                stage.draw();
+            }
+
+            @Override
+            public void resize(int width, int height) {
+                Gdx.app.debug(TAG, "mainScreen.resize(" + width + "," + height + ")");
+                stage.getViewport().update(width, height, true);
+            }
+
+            @Override
+            public void dispose() {
+                Gdx.app.debug(TAG, "mainScreen.dispose()");
+                stage.dispose();
+            }
+        };
+
+        setScreen(mainScreen);
+        Gdx.input.setInputProcessor(stage);
     }
-
-    @Override
-    public void resize(int width, int height) {
-        stage.getViewport().update(width, height, true);
-    }
-
-	@Override
-	public void render() {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        stage.act();
-        stage.draw();
-	}
 
     @Override
     public void dispose() {
-        stage.dispose();
+        super.dispose();    // calls screen.hide()
+        mainScreen.dispose();
     }
 }
