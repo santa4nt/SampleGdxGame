98b81099e966915727189bf892ff70b6f2e2bcc7
Santoso Wijaya <santoso.wijaya@gmail.com>
2015-05-30 20:43:37 -0700

    Initial commit: SampleGdxGame.
    Freshly generated by setup-gdx.jar.

---
+++ android/src/com/swijaya/samplegdx/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.swijaya.samplegdx.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.swijaya.samplegdx.SampleGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SampleGdxGame(), config);
+	}
+}
