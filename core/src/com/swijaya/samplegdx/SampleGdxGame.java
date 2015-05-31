6492ebbfd634ba1bb6418eb18434c8540c5d3694
Santoso Wijaya <santoso.wijaya@gmail.com>
2015-05-30 23:55:51 -0700

    Refactor anonymous classes into inner classes.

--- core/src/com/swijaya/samplegdx/SampleGdxGame.java
+++ core/src/com/swijaya/samplegdx/SampleGdxGame.java
@@ -14,70 +14,83 @@ public class SampleGdxGame extends Game {
 
     private static final String TAG = SampleGdxGame.class.getSimpleName();
 
+    public class BadLogicSpriteActor extends Actor {
+
+        private Texture badLogicImg;
+
+        public BadLogicSpriteActor() {
+            badLogicImg = new Texture("badlogic.jpg");
+            setPosition(0, 0);
+            setBounds(getX(), getY(), badLogicImg.getWidth(), badLogicImg.getHeight());
+
+            addListener(new InputListener() {
+                @Override
+                public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                    Gdx.app.debug(TAG, "[badLogicActor] touchDown: (" + x + "," + y + ")");
+                    return true;
+                }
+
+                @Override
+                public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                    Gdx.app.debug(TAG, "[badLogicActor] touchUp: (" + x + "," + y + ")");
+                }
+            });
+        }
+
+        @Override
+        public void draw(Batch batch, float alpha) {
+            batch.draw(badLogicImg, getX(), getY());
+        }
+
+        @Override
+        public void act(float delta) {
+            super.act(delta);
+        }
+
+    }
+
+    public class MainScreen extends ScreenAdapter {
+
+        private Stage stage;
+
+        public MainScreen(Stage stage) {
+            this.stage = stage;
+        }
+
+        @Override
+        public void render(float delta) {
+            Gdx.gl.glClearColor(1, 0, 0, 1);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+            stage.act(delta);
+            stage.draw();
+        }
+
+        @Override
+        public void resize(int width, int height) {
+            Gdx.app.debug(TAG, "mainScreen.resize(" + width + "," + height + ")");
+            stage.getViewport().update(width, height, true);
+        }
+
+        @Override
+        public void dispose() {
+            Gdx.app.debug(TAG, "mainScreen.dispose()");
+            stage.dispose();
+        }
+    }
+
     private Screen mainScreen;
-
-    private Stage stage;
-
-    private Texture badLogicImg;
-    private Actor badLogicActor;
 
     @Override
     public void create() {
         Gdx.app.setLogLevel(Application.LOG_DEBUG);
 
-        badLogicImg = new Texture("badlogic.jpg");
-        stage = new Stage(new ScreenViewport());
+        Stage stage = new Stage(new ScreenViewport());
+        stage.addActor(new BadLogicSpriteActor());
 
-        badLogicActor = new Actor() {
-            @Override
-            public void draw(Batch batch, float parentAlpha) {
-                batch.draw(badLogicImg, 0, 0);
-            }
-            @Override
-            public void act(float delta) {
-                super.act(delta);
-                setBounds(0, 0, badLogicImg.getWidth(), badLogicImg.getHeight());
-            }
-        };
-        badLogicActor.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                Gdx.app.debug(TAG, "[badLogicActor] touchDown: (" + x + "," + y + ")");
-                return true;
-            }
-
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                Gdx.app.debug(TAG, "[badLogicActor] touchUp: (" + x + "," + y + ")");
-            }
-        });
-        stage.addActor(badLogicActor);
-
-        mainScreen = new ScreenAdapter() {
-
-            @Override
-            public void render(float delta) {
-                Gdx.gl.glClearColor(1, 0, 0, 1);
-                Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-                stage.act(delta);
-                stage.draw();
-            }
-
-            @Override
-            public void resize(int width, int height) {
-                Gdx.app.debug(TAG, "mainScreen.resize(" + width + "," + height + ")");
-                stage.getViewport().update(width, height, true);
-            }
-
-            @Override
-            public void dispose() {
-                Gdx.app.debug(TAG, "mainScreen.dispose()");
-                stage.dispose();
-            }
-        };
-
+        mainScreen = new MainScreen(stage);
         setScreen(mainScreen);
+
         Gdx.input.setInputProcessor(stage);
     }
 
